9d4112b49beb7faeb36fc253c21f39b86f209649
DakshSuryavanshi <dakshsuryavanshi2003@gmail.com>
2022-10-10 00:07:39 +0530

    making javaScript work in web view

---
+++ lib/src/web_view_stack.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_webview_pro/webview_flutter.dart';
+
+class WebViewStack extends StatefulWidget {
+  const WebViewStack({super.key});
+
+  @override
+  State<WebViewStack> createState() => _WebViewStackState();
+}
+
+class _WebViewStackState extends State<WebViewStack> {
+  var loadingPercentage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        WebView(
+          initialUrl: 'https://youai.herokuapp.com/',
+          javascriptMode: JavascriptMode.unrestricted,  
+          onPageStarted: (url) {
+            setState(() {
+              loadingPercentage = 0;
+            });
+          },
+          onProgress: (progress) {
+            setState(() {
+              loadingPercentage = progress;
+            });
+          },
+          onPageFinished: (url) {
+            setState(() {
+              loadingPercentage = 100;
+            });
+          },
+        ),
+        if (loadingPercentage < 100)
+          LinearProgressIndicator(
+            value: loadingPercentage / 100.0,
+          ),
+      ],
+    );
+  }
+}
